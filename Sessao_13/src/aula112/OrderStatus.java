0d7d21fb35616a4af0d125000d05dfdb2fafa639
rodrigo tadeu_ue <rodrigo.silva@hcentrix.com>
2020-03-05 11:49:39 -0300

    created folder sessao9 and added files into the folder

---
+++ Sessao_13/src/aula112/OrderStatus.java
@@ -0,0 +1,9 @@
+package aula112;
+
+public enum OrderStatus {
+
+	PENDING_PAYMENT,
+	PROCESSING,
+	SHIPPED,
+	DELIVERED;
+}
---
+++ Sessao_13/src/aula155116/WorkerLevel.java
@@ -0,0 +1,8 @@
+package aula155116;
+
+public enum WorkerLevel {
+	
+	JUNIOR,
+	MID_LEVEL,
+	SENIOR;
+}

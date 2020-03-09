72555074e4208751142ec4d26fba6dab875a360b
Rodrigo Tadeu <rodrigotfonsecas@hotmail.com>
2020-03-09 14:31:01 -0300

    created folder sessao17 and added files into the folder

---
+++ Sessao_17/src/aula175/Exemplo.java
@@ -0,0 +1,30 @@
+package aula175;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Exemplo {
+
+	public static void main(String[] args) {
+		
+		File file = new File("C:\\temp\\in.txt");
+		Scanner sc = null;
+		try {
+			sc = new Scanner(file);
+			while (sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
+		}
+		catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+		finally {
+			if (sc != null) {
+				sc.close();
+			}
+		}
+
+	}
+
+}
